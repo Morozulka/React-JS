b75e2d498050610ad987285e738d4c63ede4f8d3
Morozulka <morosula@mail.ru>
2019-09-12 21:19:50 +0500

    ДЗ 5

---
+++ hw_5/src/app/components/Comment.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router';
+
+export default class Comment extends Component {
+  render() {
+    const { id, name, body, email } = this.props;
+
+    return (
+      <div className="card border-secondary mb-3">
+        <div className="card-header">
+          <Link to={`/comments/${id}`}>
+            {name}
+          </Link>
+
+        </div>
+        <div className="card-body text-secondary">
+          <p>{body}</p>
+          <p>{email}</p>
+        </div>
+      </div>
+    )
+  }
+}
